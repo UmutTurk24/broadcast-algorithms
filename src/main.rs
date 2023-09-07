5dd70eabd6a022dc917d2f16e832101ce3386f77
UmutTurk24 <umturk@davidson.edu>
2023-09-06 22:14:21 -0400

    Revamp

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 
-use std::io;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::{io, vec};
+use std::env;
 
-use server::{tcp_server::TCPServer, new_server};
-use tokio::net::TcpStream;
-use crate::server::Server;
-mod server;
+use tokio::fs;
+// use server::{tcp_server::TCPServer, new_server};
+use tokio::net::{TcpStream, TcpListener};
+use tokio::sync::Mutex;
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let mut map =HashMap::new();
+    
+    let addr_list = fs::read_to_string("src/config.txt")
+        .await.expect("Could not find the file");
 
+    let wrapped_list = Arc::new(addr_list);
+    
     tokio::spawn(async move {
-        let _ = new_server(server::ServerType::TCPServer).await;
+        let lst = Arc::clone(&wrapped_list);
+        for server_address in lst.lines() {
+            let server_socket = TcpListener::bind(server_address.to_string()).await;
+            map.insert(server_address, server_socket);
+        }
     });
 
+
     
-    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
 
-    // tokio::spawn(async move {
-       
+    
 
-    // });
+    // Make sure each server is aware of each other's IP? Maybe not, 
+    // Connect each server with each other
+    // 
 
-
-   
     
     Ok(())
 }
